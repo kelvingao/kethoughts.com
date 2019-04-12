4ea150b7a0814fdac385f496a782b58fec22e14f
Kelvin Gao <89156201@qq.com>
2019-04-12 14:47:38 +0000

    init a vue web

---
+++ web/src/App.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  #app
+    h1 Hello, World!
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  components: {
+   
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
